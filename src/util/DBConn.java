c1b8e3f5623c53a906ee5fdf7d29d810ed708b72
MHS <ifonlymun@naver.com>
2022-07-25 15:52:08 +0900

    db connection repair

--- src/util/DBConn.java
+++ src/util/DBConn.java
@@ -11,7 +11,7 @@ public class DBConn {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 //			connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","board", "1234");
 //			connection = DriverManager.getConnection("jdbc:oracle:thin:@human.lepelaka.net:1521:xe","USER10", "USER10");
-			connection = DriverManager.getConnection("jdbc:oracle:thin:@db.ifonlygaram.net:1521:xe","awsBoard", "1234");
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@db.ifonlygaram.net:1521:xe","awsBoard", "awsBoard");
 //			System.out.println("getConnection complete!");
 		} catch (Exception e) {
 			e.printStackTrace();
